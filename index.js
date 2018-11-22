68af341a8dff85d52e93baf59f78177a4e2829f9
Herman <hrivas5726@aol.com>
2018-11-22 03:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+
+var dnames = ['Sleepy', 'Grumpy', 'Doc', 'Dopey', 'Bashful', 'Happy', 'Sneezy'];
+
 function dwarfRollCall(dwarves) {
+  for (var i = 0; i < dnames.length; i++){
+  }
+  
+  return '1.' + newArray[0]  +'2.' + newArray[1] + '3.' + newArray[2];  
 }
 
 function summonCaptainPlanet(planeteerCalls){
