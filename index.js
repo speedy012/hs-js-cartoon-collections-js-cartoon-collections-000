ced25ad4a7b1805f516387b0924b06cf4e3babaf
Herman <hrivas5726@aol.com>
2018-12-08 18:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+
+var dnames = ['Sleepy', 'Grumpy', 'Doc', 'Dopey', 'Bashful', 'Happy', 'Sneezy'];
+
 function dwarfRollCall(dwarves) {
+  for (var i = 0; i < dnames.length; i++){
+  }
+  
+  return '1.' + newArray[0]  +'2.' + newArray[1] + '3.' + newArray[2];  
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.toUpperCase();
 }
+
+let callsUppercase = calls.map(toUpper);
+
 
 function longPlaneteerCalls(words) {
 }
