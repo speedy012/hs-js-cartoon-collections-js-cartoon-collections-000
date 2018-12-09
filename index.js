77623778a4b9067b2e94ce8c644a2d18071d281e
Herman <hrivas5726@aol.com>
2018-12-09 20:00:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+// function should take an array
+// return a string with the numbered dwarves
+
+
+var dwarves = ['Sleepy', 'Grumpy', 'Doc', 'Dopey', 'Bashful', 'Happy', 'Sneezy'];
+
 function dwarfRollCall(dwarves) {
+   newString = '';
+   for (var i = 0; i < dwarves.length; i++) {
+     dwarves.push(i);
+}
 }
 
+//accept an array of planeteer calls
+//
+
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.toUpperCase();
 }
+
+let callsUppercase = calls.map(toUpper);
+
 
 function longPlaneteerCalls(words) {
 }
